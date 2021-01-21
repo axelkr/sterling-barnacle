be0ecd22b06cc7b71a1a3361862415c12aee8fb0
Axel <axelkr.dev@google.com>
2021-01-21 21:49:44 +0100

    start fronend and backend

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,18 @@
 import { Database, Server } from 'happy-barnacle';
+import express from 'express';
+import path from 'path';
+
+const _app_folder = 'dist';
+
+const app = express();
+// ---- SERVE STATIC FILES ---- //
+app.get('*.*.*', express.static(_app_folder, {maxAge: '1y'}));
+
+app.get('/',function(req,res) {
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+app.listen(process.env.port || 4203);
+console.log('Running at Port 4200');
 
 const db = new Database("objectEventStore.db");
 const runServer = new Server(db);
