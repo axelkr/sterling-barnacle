a8f665d46e1ba8ab216c4db1b421f313ea8570ec
Axel <axelkr.dev@google.com>
2021-01-30 17:25:24 +0100

    store in user's home directory

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import { Database, Server } from 'happy-barnacle';
 import express from 'express';
 import path from 'path';
+import fs from 'fs';
 
 import configuration from './config.prod.json';
 const _app_folder = 'dist';
+
+const homedir = require('os').homedir();
+const appDirectory = path.join(homedir,'.heijunka');
+
+if (!fs.existsSync(appDirectory)){
+    fs.mkdirSync(appDirectory);
+}
+
+const dbFile = path.join(appDirectory,"objectEventStore.db");
 
 const app = express();
 // ---- SERVE STATIC FILES ---- //
@@ -15,6 +25,6 @@ app.get('/',function(req,res) {
 app.listen(configuration.frontend.port);
 console.log('Running at Port '+configuration.frontend.port);
 
-const db = new Database("objectEventStore.db");
+const db = new Database(dbFile);
 const runServer = new Server(db);
 runServer.start(configuration.backend.port);
